315ce212e2a6537e527339782c3a7597c0e3860e
therealdope <skheni02@gmail.com>
2025-02-21 12:52:50 +0530

    99

--- app.py
+++ app.py
@@ -101,4 +101,5 @@ def index():
     return render_template('index.html', result=result, error=error, operation=operation)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    from os import environ
+    app.run(host='0.0.0.0', port=int(environ.get('PORT', 5000)))
